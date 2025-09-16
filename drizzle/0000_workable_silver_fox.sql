8a1866c2beec53cd2c64abb1d96f751c7d1ffce2
dlutfulin <lutfulind1@science.regn.net>
2025-09-16 16:03:21 +0200

    fix

--- drizzle/0000_workable_silver_fox.sql
+++ drizzle/0000_workable_silver_fox.sql
@@ -1,16 +1,16 @@
 CREATE TABLE "users" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
-	"email" varchar(255) NOT NULL,
-	"username" varchar(255) NOT NULL,
-	"password" varchar(255) NOT NULL,
-	"is_active" boolean DEFAULT true,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "users_email_unique" UNIQUE("email"),
-	CONSTRAINT "users_username_unique" UNIQUE("username")
+    "id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "email" varchar(255) NOT NULL,
+    "username" varchar(255) NOT NULL,
+    "password" varchar(255) NOT NULL,
+    "is_active" boolean DEFAULT true,
+    "updated_at" timestamp DEFAULT now() NOT NULL,
+    "created_at" timestamp DEFAULT now() NOT NULL,
+    CONSTRAINT "users_email_unique" UNIQUE("email"),
+    CONSTRAINT "users_username_unique" UNIQUE("username")
 );
 
-CREATE TABLE posts (
+CREATE TABLE "posts" (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     content VARCHAR(280) NOT NULL,
